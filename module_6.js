5273b0446176f4a5d4459bc71f0e94d8ffad9eb6
Alison <145687023+Alison2n@users.noreply.github.com>
2024-11-08 11:47:52 +0300

    replace some let on const, change names

--- module_6.js
+++ module_6.js
@@ -1,33 +1,32 @@
 // 6.6.1
-let str = prompt("Введите палиндром:");
-let str1 = str.replaceAll(" ", "").toLowerCase();
-console.log(str1);
-let str2 = "";
-for (let i = str1.length - 1; i >= 0; i--) {
-  str2 += str1[i];
+const str = prompt("Введите палиндром:");
+let normalStr = str.replaceAll(" ", "").toLowerCase();
+let newStr = "";
+for (let i = normalStr.length - 1; i >= 0; i--) {
+  newStr += normalStr[i];
 }
 
-if (str1 == str2) {
+if (normalStr == newStr) {
   console.log(`Слово ${str} является палиндромом`);
 } else {
   console.log(`Слово ${str} не является палиндромом`);
 }
 
 // 6.6.2 
-const arr3 = [1, 2, 3, 1, 5, 4, 2, 3, 5, 'they', 'don\'t', 'know', 'that', 'we', 'know', 'that', 'they', 'know' ]; 
-let newArr = new Set(arr3);
+const arr = [1, 2, 3, 1, 5, 4, 2, 3, 5, 'they', 'don\'t', 'know', 'that', 'we', 'know', 'that', 'they', 'know' ]; 
+let newArr = new Set(arr);
 console.log(newArr);
 
 // 6.6.3
-let num = +prompt("Введите число:");
-let arr4 = [];
+const num = +prompt("Введите число:");
+let arr = [];
 for (let i = 0; i <= num; i++) {
-    arr4.push(i);
+    arr.push(i);
 }
-console.log(arr4);
+console.log(arr);
 
 // 6.6.4
-let boardSize = 3;
+const boardSize = 3;
 let board = [];
 while (board.length <= boardSize - 1) {
     let row = [];
@@ -49,14 +48,14 @@ const obj = {
     arr: [1, 2, 3, 4, 5],
     tom: 'there'
 };
-let arr5 = [];
+let arr = [];
 for (let key in obj) {
     if (Array.isArray(obj[key]) == true) {
         for (let i = 0; i <= obj[key].length - 1; i++) {
-            arr5.push(obj[key][i]);
+            arr.push(obj[key][i]);
         }
     } else {
-        arr5.push(obj[key]);
+        arr.push(obj[key]);
     }
 }
-console.log(arr5);
+console.log(arr);
